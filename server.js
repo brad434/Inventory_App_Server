d2c03d77331cd4c6e2adff79f836f49da336be76
Bradley Chery <bchery618@gmail.com>
2024-09-25 00:20:07 -0400

    updated the port

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
-const PORT = process.env.PORT || 5000;
+// const port = process.env.port || 5000;
+const port = 5000;
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
@@ -40,4 +41,4 @@ mongoose
   .then(() => console.log("MongoDB connected"))
   .catch(err => console.log(err));
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(port, () => console.log(`Server running on Port ${port}`));
